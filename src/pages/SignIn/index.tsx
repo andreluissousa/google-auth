a84941c20dea63ec21c09ea7d0019919328c7d3d
andreluissousa <andre.borges@tron.com.br>
2023-05-30 01:35:27 -0300

    FIx margin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,7 +6,7 @@ export function SignIn(){
         <div className="container">
             <h1>
                 MAKE YOUR <br />
-                <span id="why">WHY</span>BIGGER <br />
+                <span id="why">WHY</span> BIGGER <br />
                 THAN YOUR <br />
                 EXCUSES 
             </h1>
@@ -16,7 +16,7 @@ export function SignIn(){
                 <GoogleLogo />
                 Sign In with Google
             </button>
-            <span>                
+            <span className="button-description">                
                 Use Google authentication, and you'll make your life easier by being able to use the application without having to register.
             </span>
         </div>
